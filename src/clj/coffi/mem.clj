9ec94a68043a29b44ec2a74ba83938201b7d8daf
Kristin Rutenkolk <kristin.rutenkolk@hhu.de>
2025-07-13 16:56:07 +0200

    revert align-of and size-of memoization

--- src/clj/coffi/mem.clj
+++ src/clj/coffi/mem.clj
@@ -1176,32 +1176,18 @@
   ^Class [type]
   (java-prim-layout (or (primitive-type type) type) MemorySegment))
 
-(defn- size-of-impl
-  "The size in bytes of the given `type`."
-  ^long [type]
-  (.byteSize ^MemoryLayout (c-layout type)))
-
-(def ^:private size-of-impl-memoized (memoize size-of-impl))
-
 (defn size-of "The size in bytes of the given `type`."
   ^long [type]
-  (if (instance? MemoryLayout type)
-    (.byteSize ^MemoryLayout type)
-    (size-of-impl-memoized type)))
-
-(defn- align-of-impl
-  "The alignment in bytes of the given `type`."
-  ^long [type]
-  (.byteAlignment ^MemoryLayout (c-layout type)))
-
-(def ^:private align-of-impl-memoized (memoize align-of-impl))
+  (let [t (cond-> type
+            (not (instance? MemoryLayout type)) c-layout)]
+    (.byteSize ^MemoryLayout t)))
 
 (defn align-of
   "The alignment in bytes of the given `type`."
   ^long [type]
-  (if (instance? MemoryLayout type)
-    (.byteAlignment ^MemoryLayout type)
-    (align-of-impl-memoized type)))
+  (let [t (cond-> type
+            (not (instance? MemoryLayout type)) c-layout)]
+    (.byteAlignment ^MemoryLayout t)))
 
 (defn alloc-instance
   "Allocates a memory segment for the given `type`."
